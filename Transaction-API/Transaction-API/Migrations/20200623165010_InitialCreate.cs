758f1c13c540149c7fcbe77c1d902a09677733bb
illya.shagoferov <shagoferov@gmail.com>
2020-06-23 19:59:43 +0300

    added migrations

---
+++ Transaction-API/Transaction-API/Migrations/20200623165010_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transaction_API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    ClientName = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
--- Transaction-API/Transaction-API/Models/Transaction.cs
+++ Transaction-API/Transaction-API/Models/Transaction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,17 @@ namespace Transaction_API.Models
     {
         [Key]
         public int Id { get; set; }
+        
+        [Column(TypeName = "nvarchar(10)")]
         public string Status { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
         public string ClientName { get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
     }
 }
