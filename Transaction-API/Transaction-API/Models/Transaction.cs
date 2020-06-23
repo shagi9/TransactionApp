2bae17cbe36afccaa230c928ea65031cf9858cf0
illya.shagoferov <shagoferov@gmail.com>
2020-06-23 19:01:03 +0300

    created sceleton for api and added DbCOntext with sql connection

---
+++ Transaction-API/Transaction-API/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transaction_API.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+        public string ClientName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Transaction-API/Transaction-API/Models/TransactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transaction_API.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
