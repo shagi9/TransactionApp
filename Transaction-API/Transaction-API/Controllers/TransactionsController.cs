4474c8d6d42259ae8e4cc6b500689172a2c30343
illya shagoferov <shagoferov@gmail.com>
2020-06-24 12:13:46 +0300

    added basic http requests

---
+++ Transaction-API/Transaction-API/Controllers/TransactionsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Transaction_API.Models;
+
+namespace Transaction_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionsController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Transactions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        // POST: api/Transactions
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTransaction", new { id = transaction.Id }, transaction);
+        }
+
+        // DELETE: api/Transactions/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Transaction>> DeleteTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+
+            return transaction;
+        }
+    }
+}
